f378c822716960dc5ac8ac0edc3a8b808dc8cde8
Asd-g <65298684+Asd-g@users.noreply.github.com>
2022-09-11 15:50:57 +0300

    Remove unused variables and functions

--- DoViBaker/lut.cpp
+++ DoViBaker/lut.cpp
@@ -83,19 +83,9 @@ Vector3 operator+(const Vector3 &lhs, const Vector3 &rhs)
 	return{ lhs[0] + rhs[0], lhs[1] + rhs[1], lhs[2] + rhs[2] };
 }
 
-Vector3 operator-(const Vector3 &lhs, const Vector3 &rhs)
-{
-	return{ lhs[0] - rhs[0], lhs[1] - rhs[1], lhs[2] - rhs[2] };
-}
-
 Vector3 operator*(float lhs, const Vector3 &rhs)
 {
 	return{ lhs * rhs[0], lhs * rhs[1], lhs * rhs[2] };
-}
-
-Vector3 operator*(const Vector3 &lhs, float rhs)
-{
-	return rhs * lhs;
 }
 
 template <class T>
